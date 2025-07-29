9dbcf885037fb23251678493c74fca4b01b23119
Josh <49860579+FootlockerNZ@users.noreply.github.com>
2025-07-29 20:49:11 +1200

    Create __init__.py

---
+++ Harvester/__init__.py
@@ -0,0 +1,7 @@
+from harvester import browser, fetch, server
+from harvester.browser import BrowserEnum
+from harvester.server import CaptchaKindEnum, Harvester
+
+__version__ = '2.2.0'
+
+__all__ = ['browser', 'server', 'fetch', 'CaptchaKindEnum', 'BrowserEnum', 'Harvester']
